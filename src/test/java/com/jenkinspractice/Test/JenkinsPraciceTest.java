eae3e855fcdc4fde9410f082914feb6be07b7613
Niraj <Niraj@DESKTOP-RB30UFV>
2022-05-30 18:23:40 +0530

    first commit

---
+++ src/test/java/com/jenkinspractice/Test/JenkinsPraciceTest.java
@@ -0,0 +1,11 @@
+package com.jenkinspractice.Test;
+
+public class JenkinsPraciceTest {
+	
+	public void jenkinsPracticeTest()
+	{
+		System.out.println("Test is run successfully in jenkins");
+		System.out.println("It is for jenkins only");
+	}
+
+}
---
+++ src/test/java/com/jenkinspractice/Test/TestngPractice01Test.java
@@ -0,0 +1,25 @@
+package com.jenkinspractice.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngPractice01Test {
+	
+	@Test
+	public void practiceTest()
+	{
+		System.out.println("Test is running");
+	}
+	
+	@Test
+	public void practice01Test()
+	{
+		System.out.println("Test 01 is running");
+	}
+	
+	@Test
+	public void practice02Test()
+	{
+		System.out.println("Test 02 is running");
+	}
+
+}
